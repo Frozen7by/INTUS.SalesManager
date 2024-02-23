3a1cbc3eb31b1dcc0286c64b7a44d5681122ebcf
Zahar Zhevnyak <z.zhevnyak@itransition.com>
2024-02-23 16:33:57 +0300

    [INTUS-0] LookupActions

--- src/Api/INTUS.SalesManager.Api.Web/Controllers/LookupsController.cs
+++ src/Api/INTUS.SalesManager.Api.Web/Controllers/LookupsController.cs
@@ -16,10 +16,51 @@ public class LookupsController : Controller
         _mediator = mediator;
     }
 
-
     [HttpGet("elementTypes")]
     public Task<List<LookupDto>> GetTypes()
     {
         return _mediator.Send(new GetTypes.Request());
+    }
+
+    [HttpGet("states")]
+    public Task<List<LookupDto>> GetStates()
+    {
+        return _mediator.Send(new GetStates.Request());
+    }
+
+    [HttpPost("elementTypes")]
+    public Task AddType([FromBody] AddType.Request request)
+    {
+        return _mediator.Send(request);
+    }
+
+    [HttpPost("states")]
+    public Task AddState([FromBody] AddState.Request request)
+    {
+        return _mediator.Send(request);
+    }
+
+    [HttpPut("elementTypes")]
+    public Task UpdateType([FromBody] UpdateType.Request request)
+    {
+        return _mediator.Send(request);
+    }
+
+    [HttpPut("states")]
+    public Task UpdateState([FromBody] UpdateState.Request request)
+    {
+        return _mediator.Send(request);
+    }
+
+    [HttpDelete("elementTypes")]
+    public Task RemoveType([FromBody] RemoveType.Request request)
+    {
+        return _mediator.Send(request);
+    }
+
+    [HttpDelete("states")]
+    public Task RemoveState([FromBody] RemoveState.Request request)
+    {
+        return _mediator.Send(request);
     }
 }
