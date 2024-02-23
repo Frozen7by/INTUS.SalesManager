5d2042cd9b658041bc3dc326f3b4a796cf745b41
Zahar Zhevnyak <z.zhevnyak@itransition.com>
2024-02-23 16:01:40 +0300

    [INTUS-0] Pull OrderService implementation to interface

--- src/Domain/INTUS.SalesManager.Domain.Services/Orders/IOrderService.cs
+++ src/Domain/INTUS.SalesManager.Domain.Services/Orders/IOrderService.cs
@@ -4,7 +4,13 @@ namespace INTUS.SalesManager.Domain.Services.Orders;
 
 public interface IOrderService
 {
+    Task<long> AddOrder(OrderDto orderDto, CancellationToken cancellationToken);
+
     Task<OrderDto> GetOrder(long id, CancellationToken cancellationToken);
 
     Task<List<OrderListDto>> GetOrders(CancellationToken cancellationToken);
+
+    Task RemoveOrder(OrderDto orderDto, CancellationToken cancellationToken);
+
+    Task UpdateOrder(OrderDto orderDto, CancellationToken cancellationToken);
 }
